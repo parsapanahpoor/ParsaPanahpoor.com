0ba4b82c89e59389112eb303940e09550346f5b6
parsapanahpoor <80142438+parsapanahpoor@users.noreply.github.com>
2021-05-25 21:38:16 +0430

    initial Generic Repository Interface

--- DataContext/Repository/IRepository.cs
+++ DataContext/Repository/IRepository.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataContext.Repository
 {
-    public  interface IRepository
+    public interface IRepository<TEntity> where TEntity : class
     {
+
+        #region Public Definition Functions
+
+        //-------Definition Public Functions Models---------//
+
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(object Id);
+        void Delete(TEntity entity);
+        void Delete(Expression<Func<TEntity, bool>> where);
+
+        TEntity GetById(object Id);
+        IEnumerable<TEntity> GetAll();
+        TEntity Get(Expression<Func<TEntity, bool>> where);
+        IEnumerable<TEntity> GetMany(Expression<Func<TEntity, bool>> where);
+
+
+        #endregion
+
+
     }
 }
