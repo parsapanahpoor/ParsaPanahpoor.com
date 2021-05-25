8c7f5886af192dbc38f4e7f1768392984a54dd05
parsapanahpoor <80142438+parsapanahpoor@users.noreply.github.com>
2021-05-25 16:01:40 +0430

    initial first changes on unitOfwork

--- DataContext/UnitOfWork/IUnitOfWork.cs
+++ DataContext/UnitOfWork/IUnitOfWork.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DataContext.UnitOfWork
 {
-    public  interface IUnitOfWork
+    public  interface IUnitOfWork<TContext> : IDisposable where TContext : DbContext
     {
+        void SaveChangesDB();
+        Task<int> SaveChangesDBAsync();
     }
+
+
 }
--- DataContext/UnitOfWork/UnitOfWork.cs
+++ DataContext/UnitOfWork/UnitOfWork.cs
@@ -1,10 +1,75 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DataContext.UnitOfWork
 {
-    public   class UnitOfWork
+    public class UnitOfWork<TContext> : IUnitOfWork<TContext> where TContext : DbContext, new()
     {
+     
+
+
+        #region Fileds
+        protected readonly DbContext db;
+
+        #endregion
+
+
+
+        #region Ctor
+        public UnitOfWork()
+        {
+            db = new TContext();
+        }
+
+        #endregion
+
+
+
+
+        #region Implement
+
+        public void SaveChangesDB()
+        {
+            db.SaveChanges();
+        }
+
+        public Task<int> SaveChangesDBAsync()
+        {
+            return db.SaveChangesAsync();
+        }
+
+        #endregion
+
+
+
+
+        #region Dispose
+
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+
+        #endregion
+
+
     }
 }
