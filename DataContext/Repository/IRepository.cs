1708d7e70ac6cfbd6803641c6e03fb43077cfed1
parsapanahpoor <80142438+parsapanahpoor@users.noreply.github.com>
2021-05-25 15:44:39 +0430

    initial repository classes

---
+++ DataContext/Repository/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataContext.Repository
+{
+    public  interface IRepository
+    {
+    }
+}
---
+++ DataContext/Repository/Repository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataContext.Repository
+{
+    public  class Repository
+    {
+    }
+}
